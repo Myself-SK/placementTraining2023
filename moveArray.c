43637854f8f89f90f4c593f49e78d9d1a5f009cb
ShivaKumar <mail.shivaKumar03@gmail.com>
2023-04-24 16:00:04 +0530

    Move Array Elements

---
+++ moveArray.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void move(int a[],int* l,int* r){
+	int temp;
+		while(l<r){
+		if(*l>0 && *r>0)
+			r--;
+		if(*l<0 && *r<0)
+			l++;
+		if(*l>0 && *r<0){
+			temp=*r;
+			*r=*l;
+			*l=temp;
+			l++;
+			r--;
+		}
+		if(*l<0 && *r>0){
+			l++;
+			r--;
+		}
+	}
+
+}
+void main(){
+	int n;
+	scanf("%d",&n);
+	int a[n],i,temp;
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	int *l,*r;
+	l=a;
+	r=&a[n-1];
+	move(a,l,r);
+	for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+}
